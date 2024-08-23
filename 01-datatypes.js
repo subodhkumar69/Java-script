5e13db6f59c0871e5064a1f656d6190c27b03122
subodhkumar69 <subodhkumar70339@gmail.com>
2024-08-23 09:35:50 +0000

    datatypes

---
+++ 01-datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version
+// alert(3 + 3) we are using node js not browser //
+let name = "subodh";
+let age = 18
+let isLoggedIn = false
+
+//number=> 2 to 53//
+//bigint => //
+//string => ""
+//boolean=> true/false
+//null => standalone value
+//undefined =>
+    //symbol => uniques 
+
+
+//object =>
+    console.log(typeof subodh);
+
